aeea64232ae1e3479387bf496b107f291c4ffbd1
spidermastermind <bstacey37@gmail.com>
2019-01-05 23:05:20 +0000

    added axios, issue with asynch binding

--- app/javascript/packs/wishlist/components/landingPage.jsx
+++ app/javascript/packs/wishlist/components/landingPage.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 import '../assets/stylesheets/wishlist/list'
 import List from './lists/list.jsx'
 
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lists: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('api/v1/lists.json')
+      .then(response => {
+        this.setState(response.data)
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
     return(
       <div>
-        <List />
+        {this.state.lists.map(list =>
+          <List 
+            key={list.id}
+            listData={list.data}
+          />
+        )}
       </div>
     )
   }
 }
-
 
 export default LandingPage
--- app/javascript/packs/wishlist/components/lists/list.jsx
+++ app/javascript/packs/wishlist/components/lists/list.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import Item from './items/item.jsx';
 
 class List extends React.Component {
-  render() {
+  constructor(props) {
+    super(props)
+  }
+
+  render(props) {
     return(
       <div className={'list container border border-primary'}>
-        <h2 className="title">Wishlist 1</h2>
+        <h2 className="title">Wishlist X</h2>
         <h4>Created: then</h4>
         <h4>Last updated: now</h4>
         <ul>
